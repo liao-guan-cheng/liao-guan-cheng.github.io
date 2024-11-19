0b6835a4749635ca9363c79e97d5d6a5cde52ba3
liao <cl930129@gmail.com>
2024-11-19 09:48:22 +0800

    Bootstrap 測試

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
 document.getElementById("learn-more-btn").addEventListener("click", function () {
     const extraInfo = document.getElementById("extra-info");
-    if (extraInfo.style.display === "none") {
-        extraInfo.style.display = "block";
-    } else {
-        extraInfo.style.display = "none";
-    }
+    extraInfo.classList.toggle("d-none");
 });
